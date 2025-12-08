3119beff426b447d90847becfcbe02f4e66479de
DangVuDev <vuvandang19082004@gmail.com>
2025-12-08 22:19:26 +0700

    update: Update lestion 5, 6

--- lessons/bai6_1/test.ts
+++ lessons/bai6_1/test.ts
@@ -1,17 +1,397 @@
 import { ethers } from "ethers";
 
 async function main() {
-  const provider = new ethers.JsonRpcProvider("https://eth-sepolia.public.blastapi.io");
+  const provider = new ethers.JsonRpcProvider("https://eth-sepolia.g.alchemy.com/v2/2W2pZ23QwENfN4wbgBfmVMslzHE7wLh_");
 
   const abi = [
-    ""
+	{
+		"inputs": [
+			{
+				"internalType": "address",
+				"name": "spender",
+				"type": "address"
+			},
+			{
+				"internalType": "uint256",
+				"name": "value",
+				"type": "uint256"
+			}
+		],
+		"name": "approve",
+		"outputs": [
+			{
+				"internalType": "bool",
+				"name": "",
+				"type": "bool"
+			}
+		],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"stateMutability": "nonpayable",
+		"type": "constructor"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "address",
+				"name": "spender",
+				"type": "address"
+			},
+			{
+				"internalType": "uint256",
+				"name": "allowance",
+				"type": "uint256"
+			},
+			{
+				"internalType": "uint256",
+				"name": "needed",
+				"type": "uint256"
+			}
+		],
+		"name": "ERC20InsufficientAllowance",
+		"type": "error"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "address",
+				"name": "sender",
+				"type": "address"
+			},
+			{
+				"internalType": "uint256",
+				"name": "balance",
+				"type": "uint256"
+			},
+			{
+				"internalType": "uint256",
+				"name": "needed",
+				"type": "uint256"
+			}
+		],
+		"name": "ERC20InsufficientBalance",
+		"type": "error"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "address",
+				"name": "approver",
+				"type": "address"
+			}
+		],
+		"name": "ERC20InvalidApprover",
+		"type": "error"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "address",
+				"name": "receiver",
+				"type": "address"
+			}
+		],
+		"name": "ERC20InvalidReceiver",
+		"type": "error"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "address",
+				"name": "sender",
+				"type": "address"
+			}
+		],
+		"name": "ERC20InvalidSender",
+		"type": "error"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "address",
+				"name": "spender",
+				"type": "address"
+			}
+		],
+		"name": "ERC20InvalidSpender",
+		"type": "error"
+	},
+	{
+		"anonymous": false,
+		"inputs": [
+			{
+				"indexed": true,
+				"internalType": "address",
+				"name": "owner",
+				"type": "address"
+			},
+			{
+				"indexed": true,
+				"internalType": "address",
+				"name": "spender",
+				"type": "address"
+			},
+			{
+				"indexed": false,
+				"internalType": "uint256",
+				"name": "value",
+				"type": "uint256"
+			}
+		],
+		"name": "Approval",
+		"type": "event"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "address",
+				"name": "to",
+				"type": "address"
+			},
+			{
+				"internalType": "uint256",
+				"name": "value",
+				"type": "uint256"
+			}
+		],
+		"name": "transfer",
+		"outputs": [
+			{
+				"internalType": "bool",
+				"name": "",
+				"type": "bool"
+			}
+		],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"anonymous": false,
+		"inputs": [
+			{
+				"indexed": true,
+				"internalType": "address",
+				"name": "from",
+				"type": "address"
+			},
+			{
+				"indexed": true,
+				"internalType": "address",
+				"name": "to",
+				"type": "address"
+			},
+			{
+				"indexed": false,
+				"internalType": "uint256",
+				"name": "value",
+				"type": "uint256"
+			}
+		],
+		"name": "Transfer",
+		"type": "event"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "address",
+				"name": "from",
+				"type": "address"
+			},
+			{
+				"internalType": "address",
+				"name": "to",
+				"type": "address"
+			},
+			{
+				"internalType": "uint256",
+				"name": "value",
+				"type": "uint256"
+			}
+		],
+		"name": "transferFrom",
+		"outputs": [
+			{
+				"internalType": "bool",
+				"name": "",
+				"type": "bool"
+			}
+		],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "address",
+				"name": "owner",
+				"type": "address"
+			},
+			{
+				"internalType": "address",
+				"name": "spender",
+				"type": "address"
+			}
+		],
+		"name": "allowance",
+		"outputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "address",
+				"name": "account",
+				"type": "address"
+			}
+		],
+		"name": "balanceOf",
+		"outputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "decimals",
+		"outputs": [
+			{
+				"internalType": "uint8",
+				"name": "",
+				"type": "uint8"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "name",
+		"outputs": [
+			{
+				"internalType": "string",
+				"name": "",
+				"type": "string"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "symbol",
+		"outputs": [
+			{
+				"internalType": "string",
+				"name": "",
+				"type": "string"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "totalSupply",
+		"outputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	}
   ];
-  const contractAddress = "0x41d0Ad4E6227062B59CA673B418Fc00E3A2d10Cf"; // Replace with your contract address
+
+
+  const contractAddress = "0x995cbF88F691FCc800E686908566D14f69Bf4c75"; 
+  const deployerAddress = "0x7eff82613404ae7bce622ea1c927214e30f2285e";
   const contract = new ethers.Contract(contractAddress, abi, provider);
 
   /**
    * Get the current balance of deployer
    */
+
+  const blanceOfDeployer = await contract.balanceOf(deployerAddress);
+
+  console.log('Balance of deployer: ', blanceOfDeployer.toString());
 }
 
 main().catch(console.error);
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//  RESULT - RESULT - RESULT - RESULT - RESULT - RESULT - RESULT - RESULT - RESULT - RESULT - RESULT - RESULT - RESULT
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+      // PS C:\Users\vuvan\OneDrive\Documents\GitHub\-AC-Blockchain-Developer-Foundations-> npx ts-node run.ts bai6_1
+      // ✅ Đã chạy xong bài: bai6_1
+      // Balance of deployer:  1000000000000000000000000
+      // PS C:\Users\vuvan\OneDrive\Documents\GitHub\-AC-Blockchain-Developer-Foundations-> 
+
+
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// CONTRACT - CONTRACT - CONTRACT - CONTRACT - CONTRACT - CONTRACT - CONTRACT - CONTRACT - CONTRACT - CONTRACT - CONTRACT 
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+      // SPDX-License-Identifier: MIT
+      // pragma solidity ^0.8.28;
+
+      // import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+
+      // contract MyToken is ERC20 {
+      //     uint256 constant TOTAL_TOKEN = 1_000_000 * 10 ** 18;
+
+      //     constructor() ERC20("J97Token", "J97") {
+      //         _mint(msg.sender, TOTAL_TOKEN);
+      //     }
+      // }
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// DEPLOY - DEPLOY -DEPLOY -DEPLOY -DEPLOY -DEPLOY -DEPLOY -DEPLOY -DEPLOY -DEPLOY -DEPLOY -DEPLOY -DEPLOY -DEPLOY -DEPLOY
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+      // PS D:\Blockchain\ac-hardhat-template> npx hardhat deploy --network sepolia --tags mytoken
+      // Generating typings for: 8 artifacts in dir: typechain for target: ethers-v6
+      // Successfully generated 34 typings!
+      // Compiled 6 Solidity files successfully (evm target: paris).
+      //  ·------------------------|--------------------------------|--------------------------------·
+      //  |  Solc version: 0.8.28  ·  Optimizer enabled: true       ·  Runs: 1000                    │
+      //  ·························|································|·································
+      //  |  Contract Name         ·  Deployed size (KiB) (change)  ·  Initcode size (KiB) (change)  │
+      //  ·························|································|·································
+      //  |  Counter               ·                 0.246 (0.000)  ·                 0.271 (0.000)  │
+      //  ·························|································|·································
+      //  |  MyToken               ·                      1.744 ()  ·                      2.692 ()  │
+      //  ·------------------------|--------------------------------|--------------------------------·
+      // ====================
+      // sepolia
+      // ====================
+      // ====================
+      // Deploy MyToken Contract
+      // ====================
+      // deploying "MyToken" (tx: 0x8553a3629dffd1e7364f40270d70da20e8df515cd0b09695d8d1c38204196c1e)...: deployed at 0x995cbF88F691FCc800E686908566D14f69Bf4c75 with 542474 gas
+      // PS D:\Blockchain\ac-hardhat-template>
+
+
+
+
--- lessons/bai6_3/test.ts
+++ lessons/bai6_3/test.ts
@@ -1,17 +1,648 @@
 import { ethers } from "ethers";
 
 async function main() {
-  const provider = new ethers.JsonRpcProvider("https://eth-sepolia.public.blastapi.io");
+  const provider = new ethers.JsonRpcProvider("https://eth-sepolia.g.alchemy.com/v2/2W2pZ23QwENfN4wbgBfmVMslzHE7wLh_");
 
+  const deployerAddress = "0x7eff82613404ae7bce622ea1c927214e30f2285e";
+  const privateKey = "a267c5c70821b9382dee97afcf***************6db4525461db49c6d542d4d0e";
+  const signer = new ethers.Wallet(privateKey, provider);
+  
   const abi = [
-    ""
+	{
+		"inputs": [],
+		"stateMutability": "nonpayable",
+		"type": "constructor"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "address",
+				"name": "sender",
+				"type": "address"
+			},
+			{
+				"internalType": "uint256",
+				"name": "tokenId",
+				"type": "uint256"
+			},
+			{
+				"internalType": "address",
+				"name": "owner",
+				"type": "address"
+			}
+		],
+		"name": "ERC721IncorrectOwner",
+		"type": "error"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "address",
+				"name": "operator",
+				"type": "address"
+			},
+			{
+				"internalType": "uint256",
+				"name": "tokenId",
+				"type": "uint256"
+			}
+		],
+		"name": "ERC721InsufficientApproval",
+		"type": "error"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "address",
+				"name": "approver",
+				"type": "address"
+			}
+		],
+		"name": "ERC721InvalidApprover",
+		"type": "error"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "address",
+				"name": "operator",
+				"type": "address"
+			}
+		],
+		"name": "ERC721InvalidOperator",
+		"type": "error"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "address",
+				"name": "owner",
+				"type": "address"
+			}
+		],
+		"name": "ERC721InvalidOwner",
+		"type": "error"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "address",
+				"name": "receiver",
+				"type": "address"
+			}
+		],
+		"name": "ERC721InvalidReceiver",
+		"type": "error"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "address",
+				"name": "sender",
+				"type": "address"
+			}
+		],
+		"name": "ERC721InvalidSender",
+		"type": "error"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "uint256",
+				"name": "tokenId",
+				"type": "uint256"
+			}
+		],
+		"name": "ERC721NonexistentToken",
+		"type": "error"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "address",
+				"name": "owner",
+				"type": "address"
+			}
+		],
+		"name": "OwnableInvalidOwner",
+		"type": "error"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "address",
+				"name": "account",
+				"type": "address"
+			}
+		],
+		"name": "OwnableUnauthorizedAccount",
+		"type": "error"
+	},
+	{
+		"anonymous": false,
+		"inputs": [
+			{
+				"indexed": true,
+				"internalType": "address",
+				"name": "owner",
+				"type": "address"
+			},
+			{
+				"indexed": true,
+				"internalType": "address",
+				"name": "approved",
+				"type": "address"
+			},
+			{
+				"indexed": true,
+				"internalType": "uint256",
+				"name": "tokenId",
+				"type": "uint256"
+			}
+		],
+		"name": "Approval",
+		"type": "event"
+	},
+	{
+		"anonymous": false,
+		"inputs": [
+			{
+				"indexed": true,
+				"internalType": "address",
+				"name": "owner",
+				"type": "address"
+			},
+			{
+				"indexed": true,
+				"internalType": "address",
+				"name": "operator",
+				"type": "address"
+			},
+			{
+				"indexed": false,
+				"internalType": "bool",
+				"name": "approved",
+				"type": "bool"
+			}
+		],
+		"name": "ApprovalForAll",
+		"type": "event"
+	},
+	{
+		"anonymous": false,
+		"inputs": [
+			{
+				"indexed": true,
+				"internalType": "address",
+				"name": "previousOwner",
+				"type": "address"
+			},
+			{
+				"indexed": true,
+				"internalType": "address",
+				"name": "newOwner",
+				"type": "address"
+			}
+		],
+		"name": "OwnershipTransferred",
+		"type": "event"
+	},
+	{
+		"anonymous": false,
+		"inputs": [
+			{
+				"indexed": true,
+				"internalType": "address",
+				"name": "from",
+				"type": "address"
+			},
+			{
+				"indexed": true,
+				"internalType": "address",
+				"name": "to",
+				"type": "address"
+			},
+			{
+				"indexed": true,
+				"internalType": "uint256",
+				"name": "tokenId",
+				"type": "uint256"
+			}
+		],
+		"name": "Transfer",
+		"type": "event"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "address",
+				"name": "to",
+				"type": "address"
+			},
+			{
+				"internalType": "uint256",
+				"name": "tokenId",
+				"type": "uint256"
+			}
+		],
+		"name": "approve",
+		"outputs": [],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "address",
+				"name": "owner",
+				"type": "address"
+			}
+		],
+		"name": "balanceOf",
+		"outputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "uint256",
+				"name": "tokenId",
+				"type": "uint256"
+			}
+		],
+		"name": "getApproved",
+		"outputs": [
+			{
+				"internalType": "address",
+				"name": "",
+				"type": "address"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "address",
+				"name": "owner",
+				"type": "address"
+			},
+			{
+				"internalType": "address",
+				"name": "operator",
+				"type": "address"
+			}
+		],
+		"name": "isApprovedForAll",
+		"outputs": [
+			{
+				"internalType": "bool",
+				"name": "",
+				"type": "bool"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "address",
+				"name": "to",
+				"type": "address"
+			}
+		],
+		"name": "mint",
+		"outputs": [],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "name",
+		"outputs": [
+			{
+				"internalType": "string",
+				"name": "",
+				"type": "string"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "nextTokenId",
+		"outputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "owner",
+		"outputs": [
+			{
+				"internalType": "address",
+				"name": "",
+				"type": "address"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "uint256",
+				"name": "tokenId",
+				"type": "uint256"
+			}
+		],
+		"name": "ownerOf",
+		"outputs": [
+			{
+				"internalType": "address",
+				"name": "",
+				"type": "address"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "renounceOwnership",
+		"outputs": [],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "address",
+				"name": "from",
+				"type": "address"
+			},
+			{
+				"internalType": "address",
+				"name": "to",
+				"type": "address"
+			},
+			{
+				"internalType": "uint256",
+				"name": "tokenId",
+				"type": "uint256"
+			}
+		],
+		"name": "safeTransferFrom",
+		"outputs": [],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "address",
+				"name": "from",
+				"type": "address"
+			},
+			{
+				"internalType": "address",
+				"name": "to",
+				"type": "address"
+			},
+			{
+				"internalType": "uint256",
+				"name": "tokenId",
+				"type": "uint256"
+			},
+			{
+				"internalType": "bytes",
+				"name": "data",
+				"type": "bytes"
+			}
+		],
+		"name": "safeTransferFrom",
+		"outputs": [],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "address",
+				"name": "operator",
+				"type": "address"
+			},
+			{
+				"internalType": "bool",
+				"name": "approved",
+				"type": "bool"
+			}
+		],
+		"name": "setApprovalForAll",
+		"outputs": [],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "bytes4",
+				"name": "interfaceId",
+				"type": "bytes4"
+			}
+		],
+		"name": "supportsInterface",
+		"outputs": [
+			{
+				"internalType": "bool",
+				"name": "",
+				"type": "bool"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "symbol",
+		"outputs": [
+			{
+				"internalType": "string",
+				"name": "",
+				"type": "string"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "uint256",
+				"name": "tokenId",
+				"type": "uint256"
+			}
+		],
+		"name": "tokenURI",
+		"outputs": [
+			{
+				"internalType": "string",
+				"name": "",
+				"type": "string"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "address",
+				"name": "from",
+				"type": "address"
+			},
+			{
+				"internalType": "address",
+				"name": "to",
+				"type": "address"
+			},
+			{
+				"internalType": "uint256",
+				"name": "tokenId",
+				"type": "uint256"
+			}
+		],
+		"name": "transferFrom",
+		"outputs": [],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "address",
+				"name": "newOwner",
+				"type": "address"
+			}
+		],
+		"name": "transferOwnership",
+		"outputs": [],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	}
   ];
-  const contractAddress = "0x41d0Ad4E6227062B59CA673B418Fc00E3A2d10Cf"; // Replace with your contract address
-  const contract = new ethers.Contract(contractAddress, abi, provider);
+
+  const contractAddress = "0x30C6F4AE901fe8A80A8ec60aAfD6175aB3C9e576"; // Replace with your contract address
+  const contract = new ethers.Contract(contractAddress, abi, signer);
 
   /**
    * Mint a NFT to deployer
    */
+  // send transaction to mint nft 0 for deployer address
+
+  const mintTx = await contract.mint(deployerAddress);
+  await mintTx.wait();
+
+  // Get token with id = 0
+
+  const ownerToken = await contract.ownerOf(0);
+
+  console.log("Token 0 become to: ",ownerToken);
+
+
 }
 
 main().catch(console.error);
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//  RESULT - RESULT - RESULT - RESULT - RESULT - RESULT - RESULT - RESULT - RESULT - RESULT - RESULT - RESULT - RESULT
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+      // PS C:\Users\vuvan\OneDrive\Documents\GitHub\-AC-Blockchain-Developer-Foundations-> npx ts-node run.ts bai6_3
+      // ✅ Đã chạy xong bài: bai6_3
+      // Token 0 become to:  0x7EFf82613404AE7Bce622EA1C927214E30F2285e
+      // PS C:\Users\vuvan\OneDrive\Documents\GitHub\-AC-Blockchain-Developer-Foundations-> 
+
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// CONTRACT - CONTRACT - CONTRACT - CONTRACT - CONTRACT - CONTRACT - CONTRACT - CONTRACT - CONTRACT - CONTRACT - CONTRACT 
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+      // SPDX-License-Identifier: MIT
+      // pragma solidity ^0.8.28;
+
+      // import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+      // import "@openzeppelin/contracts/access/Ownable.sol";
+
+      // contract MyNFT is ERC721 , Ownable {
+      //     uint256 public nextTokenId;
+
+      //     constructor()  ERC721("DANGVUNFT", "DNFT") Ownable(msg.sender) {
+      //         nextTokenId = 0;
+      //     }
+
+      //     function mint(address to) public onlyOwner {
+      //         _safeMint(to, nextTokenId);
+      //         nextTokenId++;
+      //     }
+      // }
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// DEPLOY - DEPLOY -DEPLOY -DEPLOY -DEPLOY -DEPLOY -DEPLOY -DEPLOY -DEPLOY -DEPLOY -DEPLOY -DEPLOY -DEPLOY -DEPLOY -DEPLOY
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+      // PS D:\Blockchain\ac-hardhat-template> npx hardhat deploy --network sepolia --tags mynft
+      // Generating typings for: 14 artifacts in dir: typechain for target: ethers-v6
+      // Successfully generated 52 typings!
+      // Compiled 14 Solidity files successfully (evm target: paris).
+      //  ·------------------------|--------------------------------|--------------------------------·
+      //  |  Solc version: 0.8.28  ·  Optimizer enabled: true       ·  Runs: 1000                    │
+      //  ·························|································|································
+      //  |  ERC721Utils           ·                      0.057 ()  ·                      0.085 ()  │
+      //  ·························|································|·································
+      //  |  Math                  ·                      0.057 ()  ·                      0.085 ()  │
+      //  ·························|································|·································
+      //  |  MyNFT                 ·                      4.048 ()  ·                      4.939 ()  │
+      //  ·························|································|·································
+      //  |  Panic                 ·                      0.057 ()  ·                      0.085 ()  │
+      //  ·························|································|·································
+      //  |  SafeCast              ·                      0.057 ()  ·                      0.085 ()  │
+      //  ·························|································|·································
+      //  |  SignedMath            ·                      0.057 ()  ·                      0.085 ()  │
+      //  ·························|································|·································
+      //  |  Strings               ·                      0.057 ()  ·                      0.085 ()  │
+      //  ·------------------------|--------------------------------|--------------------------------·
+      // deploying "MyNFT" (tx: 0x7cb2046ee1f0541167304b48a3db24d2b571c481d40f7b85d169b91fb4f08292)...: deployed at 0x30C6F4AE901fe8A80A8ec60aAfD6175aB3C9e576 with 1030396 gas
+      // PS D:\Blockchain\ac-hardhat-template>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
