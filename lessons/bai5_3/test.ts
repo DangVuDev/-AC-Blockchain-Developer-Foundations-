0b7dffb20c5eccd0f91dea22611f25c1e19b6b2c
DangVuDev <vuvandang19082004@gmail.com>
2026-01-16 10:51:17 +0700

    Update 5.3

--- lessons/bai5_3/test.ts
+++ lessons/bai5_3/test.ts
@@ -3,19 +3,24 @@ import { ethers } from "ethers";
 async function main() {
   const provider = new ethers.JsonRpcProvider("https://eth-sepolia.g.alchemy.com/v2/2W2pZ23QwENfN4wbgBfmVMslzHE7wLh_");
 
-  const privateKey = "a267c5c70821b9382dee*******************b4525461db49c6d542d4d0e";
+  const privateKey = "a267c5c70821b9382dee97afcf45be4f57246ff6db4525461db49c6d542d4d0e";
   const signer = new ethers.Wallet(privateKey, provider);
 // Tính năng mới increment();
   const abi = [
     "function getCount() public view returns (uint)",
     "function increment() public"
   ];
-  const contractAddress = "0x23bB9dCcb4D8c6a316fCF4767948b69021e3775c"; // Contract deploy on sepolia
+  const contractAddress = "0x725E62364CF6024bd1b681AB779692Eb8A2Df066"; // Contract deploy on sepolia
 
   const contract = new ethers.Contract(contractAddress, abi, signer);
 
-  const hashIncrementActionResult = await contract.increment();
-  console.log("Action increment hash: ", hashIncrementActionResult.toString());
+  const Currentcount = await contract.getCount();
+  console.log("Current count is:", Currentcount.toString());
+
+  const tx = await contract.increment();
+  const receipt = await tx.wait();
+  console.log("Transaction receipt:", receipt);
+  console.log("Increment action mined in tx:", JSON.stringify(tx));
 
   const count = await contract.getCount();
   console.log("Current count is:", count.toString());
